b8b7f5948f3f8a3dcc28ee4432e7d248927130b4
Orest58008 <orest58008@tuta.io>
2023-03-12 13:08:12 +0200

    adding configuration and output. first release

---
+++ README.md
@@ -0,0 +1,28 @@
+# YASS is yet another sysfetch software written in Go
+
+## Features
+* environment variables, os-release, meminfo, hostname, kernel version support
+* basic configuration with ANSI styling support
+* no dependencies
+
+## Configuration
+### ANSI styling
+* `<b>` for bold text
+* `<u>` for underline
+* `<r>` for reversed colors
+### Colors
+* `<color>` for color
+  * supported colors: black, red, green, yellow, blue, magenta, cyan and white
+* `<brcolor>` for bright version of color
+* `<bgcolor>` for background color
+* `<bgbrcolor>` for bright version of background color
+### Using variables from envvars, os-release, meminfo etc.
+* `|VARIABLE_NAME|` will be replaced with variable's value
+  * the name should be in ALL CAPS
+  * names are the same as in source(e.g. `MEMREE` for free memory); hostname is `HOSTNAME`, kernel version is `KERNEL_VERSION`
+
+## TODO
+* [ ] example configuration
+* [ ] automatic distro color detection(e.g. cyan for Arch, red for Ubuntu etc.)
+* [ ] left-side ASCII art support
+  * [ ] distro logo for ASCII art
--- main.go
+++ main.go
@@ -7,9 +7,11 @@ import (
 	"strings"
 )
 
+//map to store values later outputted
+var mainMap map[string]string
+    
 func main() {
-    //map to store values later outputted
-    mainMap := map[string]string{}
+    mainMap = make(map[string]string)
 
     //environment variables
     envVars := os.Environ()
@@ -34,10 +36,13 @@ func main() {
     //reading kernel version
     kernelVersion, err := os.ReadFile("/proc/sys/kernel/osrelease")
     if err != nil { log.Fatal(err) }
-    mainMap["KERNEL_VERSION"] = string(kernelVersion)
+    mainMap["KERNEL_VERSION"] = strings.TrimSpace(string(kernelVersion))
 
-
-    fmt.Println(mainMap)
+    //reading config and outputting final result
+    result := parseConfig("./config")
+    for i := range result {
+	fmt.Println(result[i])
+    }
 }
 
 func appendArray(array []string, destinationMap map[string]string, splitter string) {
@@ -52,4 +57,68 @@ func read(path string) []string {
     if err != nil { log.Fatal(err) }
     result := strings.Split(string(resultStr), "\n")
     return result
+}
+
+func parseConfig(pathToConfig string) []string {
+    configStr, err := os.ReadFile(pathToConfig)
+    if err != nil { log.Fatal(err) }
+    config := strings.Split(string(configStr), "\n")
+
+    //parsing basic styling
+    for i := range config {
+	//clear styles
+	config[i] = strings.ReplaceAll(config[i], "<c>", "\u001b[0m")
+	config[i] = strings.ReplaceAll(config[i], "<>", "\u001b[0m")
+        //bold, underlined and reversed
+	config[i] = strings.ReplaceAll(config[i], "<b>", "\u001b[1m")
+	config[i] = strings.ReplaceAll(config[i], "<u>", "\u001b[4m")
+	config[i] = strings.ReplaceAll(config[i], "<r>", "\u001b[7m")
+        //colors
+	config[i] = strings.ReplaceAll(config[i], "<black>", "\u001b[30m")
+	config[i] = strings.ReplaceAll(config[i], "<red>", "\u001b[31m")
+	config[i] = strings.ReplaceAll(config[i], "<green>", "\u001b[32m")
+	config[i] = strings.ReplaceAll(config[i], "<yellow>", "\u001b[33m")
+	config[i] = strings.ReplaceAll(config[i], "<blue>", "\u001b[34m")
+	config[i] = strings.ReplaceAll(config[i], "<magenta>", "\u001b[35m")
+	config[i] = strings.ReplaceAll(config[i], "<cyan>", "\u001b[36m")
+	config[i] = strings.ReplaceAll(config[i], "<white>", "\u001b[37m")
+	//bright colors
+	config[i] = strings.ReplaceAll(config[i], "<brblack>", "\u001b[30m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<brred>", "\u001b[31m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<brgreen>", "\u001b[32m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<bryellow>", "\u001b[33m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<brblue>", "\u001b[34m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<brmagenta>", "\u001b[35m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<brcyan>", "\u001b[36m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<brwhite>", "\u001b[37m;1m")
+	//background colors
+	config[i] = strings.ReplaceAll(config[i], "<bgblack>", "\u001b[40m")
+	config[i] = strings.ReplaceAll(config[i], "<bgred>", "\u001b[41m")
+	config[i] = strings.ReplaceAll(config[i], "<bggreen>", "\u001b[42m")
+	config[i] = strings.ReplaceAll(config[i], "<bgyellow>", "\u001b[43m")
+	config[i] = strings.ReplaceAll(config[i], "<bgblue>", "\u001b[44m")
+	config[i] = strings.ReplaceAll(config[i], "<bgmagenta>", "\u001b[45m")
+	config[i] = strings.ReplaceAll(config[i], "<bgcyan>", "\u001b[46m")
+	config[i] = strings.ReplaceAll(config[i], "<bgwhite>", "\u001b[47m")
+	//background bright colors
+	config[i] = strings.ReplaceAll(config[i], "<bgbrblack>", "\u001b[40m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<bgbrred>", "\u001b[41m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<bgbrgreen>", "\u001b[42m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<bgbryellow>", "\u001b[43m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<bgbrblue>", "\u001b[44m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<bgbrmagenta>", "\u001b[45m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<bgbrcyan>", "\u001b[46m;1m")
+	config[i] = strings.ReplaceAll(config[i], "<bgbrwhite>", "\u001b[47m;1m")
+    }
+
+    //parsing values
+    for i := range config {
+	line := strings.Split(config[i], "|")
+	for j := range line {
+	    val, ok := mainMap[line[j]]
+	    if ok { line[j] = val }
+	}
+	config[i] = strings.Join(line, "")
+    }
+    return config
 }
